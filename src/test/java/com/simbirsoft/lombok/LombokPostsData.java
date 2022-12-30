dbe719fed5ed7b856fd5124d04f12d5c1738b62c
a-shashkin <caezar1993@gmail.com>
2022-12-30 09:50:23 +0400

    Fixed experimental tests with Lombok.

--- src/test/java/com/simbirsoft/lombok/LombokPostsData.java
+++ src/test/java/com/simbirsoft/lombok/LombokPostsData.java
@@ -3,19 +3,12 @@ package com.simbirsoft.lombok;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class LombokPostsGetRequests {
-
-    private List<Post> posts;
-
-    public class Post {
+public class LombokPostsData {
 
         private Integer id;
         private Integer userId;
         private String title;
         private String body;
-    }
 }
--- src/test/java/com/simbirsoft/tests/PostsTests.java
+++ src/test/java/com/simbirsoft/tests/PostsTests.java
@@ -1,6 +1,6 @@
 package com.simbirsoft.tests;
 
-import com.simbirsoft.lombok.LombokPostsGetRequests;
+import com.simbirsoft.lombok.LombokPostsData;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +12,7 @@ import java.util.Properties;
 import static io.restassured.RestAssured.*;
 import static com.simbirsoft.specs.Specs.requestSpecification;
 import static com.simbirsoft.specs.Specs.responseSpecification;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PostsTests extends TestBase {
 
@@ -46,16 +47,54 @@ public class PostsTests extends TestBase {
     }
 
     @Test
+    void getSpecificUserTest() throws IOException {
+
+        properties.load(file);
+
+        Response response =
+                given().
+                        log().all().
+                        header("user-agent", " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " +
+                                "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15").
+                        get("/posts/2").
+                        then().
+                        log().all().
+                        statusCode(200).
+                        extract().response();
+
+        Integer id = response.path("id");
+
+        assertEquals(2, id);
+    }
+
+    @Test
     void lombokGetAllPostsTest() {
 
-        LombokPostsGetRequests[] postsGetRequestsData =
+        LombokPostsData[] postsGetRequestsData =
                 given().
                         spec(requestSpecification).
                 when().
                         get("/posts").
                 then().
                         spec(responseSpecification).
+                        log().body().
+                        extract().as(LombokPostsData[].class);
+
+    }
+
+    @Test
+    void lombokGetSpecificPostTest() {
+
+        LombokPostsData postsGetRequestsData =
+                given().
+                        spec(requestSpecification).
+                when().
+                        get("/posts/2").
+                then().
+                        spec(responseSpecification).
                         log().all().
-                        extract().as(LombokPostsGetRequests[].class);
+                        extract().as(LombokPostsData.class);
+
+        assertEquals(2, postsGetRequestsData.getId());
     }
 }
