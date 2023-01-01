7dc883ac67565b220a24ac442246cb470f23ad58
a-shashkin <caezar1993@gmail.com>
2023-01-01 17:38:27 +0400

    Realized 3 out of 8 albums' tests.

--- src/test/java/com/simbirsoft/tests/AlbumsTests.java
+++ src/test/java/com/simbirsoft/tests/AlbumsTests.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 import static com.simbirsoft.specs.Specs.requestSpecification;
 import static com.simbirsoft.specs.Specs.responseSpecification;
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -34,28 +36,58 @@ public class AlbumsTests extends TestBase {
     }
 
     @Test
-    void getSpecificPostTest() {
+    void getSpecificAlbumTest() {
 
         Gson gson = new Gson();
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream stream = classLoader.getResourceAsStream("testData/expectedResults/postsRequests/getPostExpectedResult.json");
+        InputStream stream = classLoader.getResourceAsStream("testData/expectedResults/albumsRequests/getAlbumExpectedResult.json");
         JsonReader jsonReader = new JsonReader(new InputStreamReader(stream));
         jsonReader.setLenient(true);
-        LombokPostsData expectedData = gson.fromJson(jsonReader, LombokPostsData.class);
+        LombokAlbumsData expectedData = gson.fromJson(jsonReader, LombokAlbumsData.class);
 
-        LombokPostsData postsGetRequestsData =
+        LombokAlbumsData albumsGetRequestData =
                 given().
                         spec(requestSpecification).
                         when().
-                        get("/posts/" + expectedData.getId()).
+                        get("/albums/" + expectedData.getId()).
                         then().
                         spec(responseSpecification).
                         log().all().
-                        extract().as(LombokPostsData.class);
+                        extract().as(LombokAlbumsData.class);
 
-        assertEquals(expectedData.getId(), postsGetRequestsData.getId());
-        assertEquals(expectedData.getUserId(), postsGetRequestsData.getUserId());
-        assertEquals(expectedData.getTitle(), postsGetRequestsData.getTitle());
-        assertEquals(expectedData.getBody(), postsGetRequestsData.getBody());
+        assertEquals(expectedData.getId(), albumsGetRequestData.getId());
+        assertEquals(expectedData.getUserId(), albumsGetRequestData.getUserId());
+        assertEquals(expectedData.getTitle(), albumsGetRequestData.getTitle());
+    }
+
+    @Test
+    void getSpecificUserAlbums() {
+
+        Gson gson = new Gson();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = classLoader.getResourceAsStream("testData/expectedResults/albumsRequests/getSpecificUserAlbums.json");
+        JsonReader jsonReader = new JsonReader(new InputStreamReader(stream));
+        jsonReader.setLenient(true);
+        LombokAlbumsData[] expectedData = gson.fromJson(jsonReader, LombokAlbumsData[].class);
+
+        LombokAlbumsData[] albumsGetRequestsData =
+                given().
+                        spec(requestSpecification).
+                        when().
+                        get("/users/" + expectedData[0].getUserId() + "/albums").
+                        then().
+                        spec(responseSpecification).
+                        log().body().
+                        extract().as(LombokAlbumsData[].class);
+
+        for (int i = 0; i < expectedData.length; i++) {
+            assertEquals(albumsGetRequestsData[i].getUserId(), expectedData[i].getUserId());
+            assertThat(Arrays.asList(albumsGetRequestsData)).
+                    extracting(LombokAlbumsData::getId).
+                    isNotNull();
+            assertThat(Arrays.asList(albumsGetRequestsData)).
+                    extracting(LombokAlbumsData::getTitle).
+                    isNotNull();
+        }
     }
 }
